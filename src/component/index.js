48f0064596032f32b52496f94f9c2c8c369635f7
jstsoso <jycomputer_417@163.com>
2020-04-12 18:30:39 +0800

    style: remove comment

--- src/component/index.js
+++ src/component/index.js
@@ -1,5 +1,4 @@
-// 自动全局注册 src/component 文件下的组件 => 在入口处导入此文件即可
-// https://cn.vuejs.org/v2/guide/components-registration.html#基础组件的自动化全局注册
+// 自动全局注册 src/component 文件下的组件(注意：只匹配 base-*.vue 命名的组件，其他命名方式不会注册)
 import Vue from 'vue'
 
 const requireComponent = require.context('.', false, /base-[\w-]+\.vue$/) // 注意：只匹配 base-*.vue 命名的组件
