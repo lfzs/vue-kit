d78cef84d1c269cd90b29a5c016ece8d354044e7
lfzs <jycomputer_417@163.com>
2021-03-11 22:29:12 +0800

    refactor: cache store

--- src/store/helper/Cache.js
+++ src/store/helper/Cache.js
@@ -1,30 +1,29 @@
+import { reactive } from 'vue'
+
 const isRequired = () => {
   throw new Error('id is required')
 }
 
 export default class {
-  static caches = Object.create(null)
+  static caches = new Map()
 
   static findOrCreate(id = isRequired()) {
     this.flag = this.flag || Math.random().toString(36).slice(2) // 向构造函数添加 flag 标志
-
     const key = `${this.flag}_${id}`
-    let value = this.caches[key]
 
-    if (!value) {
-      value = new this(id)
+    if (!this.caches.has(key)) {
+      const value = new this(id)
       value.id = id
-      this.caches[key] = value
+      this.caches.set(key, reactive(value)) // 将 value 值变成响应式
     }
 
-    return this.caches[key]
+    return this.caches.get(key)
   }
 
   static remove(id) {
     if (this.flag && id) {
       const key = `${this.flag}_${id}`
-      const value = this.caches[key]
-      value && delete this.caches[key]
+      this.caches.has(key) && this.caches.delete(key)
     }
   }
 }
