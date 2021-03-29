7d0cfca43a6090d93d90e33b29231f406554f404
lfzs <jycomputer_417@163.com>
2021-03-29 22:18:26 +0800

    style: loading animate

--- src/component/base/base-page-loading.vue
+++ src/component/base/base-page-loading.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="base-page-loading flex-center">
-    loading...
-  </div>
+  <div class="base-page-loading flex-center">Loading...</div>
 </template>
 
 <script>
@@ -11,7 +9,24 @@
 </script>
 
 <style lang="less" scoped>
+  @keyframes opacity {
+    0% {
+      opacity: 0.05;
+    }
+
+    50% {
+      opacity: 1;
+    }
+
+    100% {
+      opacity: 0.05;
+    }
+  }
+
   .base-page-loading {
     height: calc(100 * var(--vh));
+    font-size: 16px;
+    opacity: 0;
+    animation: opacity 3s infinite 2s;
   }
 </style>
