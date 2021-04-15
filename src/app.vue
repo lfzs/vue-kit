3bbfa5c535d1287b3c6cc88279478ef346ec066e
lfzs <jycomputer_417@163.com>
2021-04-15 16:58:49 +0800

    fix: remove global config

--- src/app.vue
+++ src/app.vue
@@ -1,18 +1,27 @@
 <template>
   <router-view v-slot="{ Component }">
-    <template v-if="currentRoute">
-      <base-page :component="Component" />
-    </template>
+
+    <suspense>
+      <component :is="Component" />
+      <template #fallback>
+        <base-page-error v-if="error" :error="error" />
+        <base-page-loading v-else />
+      </template>
+    </suspense>
+
   </router-view>
 </template>
 
 <script>
-  import { currentRoute } from '@/store'
+  import { ref, onErrorCaptured } from 'vue'
   export default {
     name: 'app',
     setup() {
+      const error = ref(null)
+      onErrorCaptured((e, instance, info) => (info === 'setup function') && (error.value = e)) // 只捕获 setup function 的 error
+
       return {
-        currentRoute,
+        error
       }
     }
   }
