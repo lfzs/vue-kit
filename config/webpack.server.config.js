f481bedf6225efb1549ee952c3645bf01e35b02a
zg <3199906514@qq.com>
2021-09-08 14:35:36 +0800

    feat: update pakcage

--- config/webpack.server.config.js
+++ config/webpack.server.config.js
@@ -3,7 +3,6 @@ const { merge } = require('webpack-merge')
 const base = require('./webpack.base.config')
 const nodeExternals = require('webpack-node-externals')
 
-const isDev = process.env.APP_ENV === 'development'
 const resolve = dir => require('path').join(__dirname, dir)
 
 module.exports = merge(base, {
@@ -11,10 +10,9 @@ module.exports = merge(base, {
   target: 'node',
   // 跳过 node_mdoules(运行时加载，不需要编译)
   externals: nodeExternals({ allowlist: /\.(css)$/ }),
-  devtool: isDev && 'source-map',
 
   output: {
-    filename: 'main.js',
+    filename: 'index.js',
     path: resolve('../dist/server'),
     libraryTarget: 'commonjs2',
   },
