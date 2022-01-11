92c46c3758809cb295c69a38373c2e3613b06fa1
lfzs <jycomputer_417@163.com>
2022-01-11 22:16:32 +0800

    refactor: axios

--- src/util/axios/interceptor/handle-request.ts
+++ src/util/axios/interceptor/handle-request.ts
@@ -1,27 +1,25 @@
 import Axios, { AxiosRequestConfig } from 'axios'
-// import { authStore } from '@/store'
+import { IS_CLIENT } from '@/constant'
 
-const requestSetSignature = (() => {
+// 通过参入 sign 参数，自动取消重复的请求
+const autoCancelRequestBySign = (() => {
   const sourceMap = new Map()
   return (request: AxiosRequestConfig) => {
-    const { signature } = request
-    if (signature) {
-      sourceMap.get(signature)?.cancel()
+    const { sign } = request
+    if (sign) {
+      sourceMap.get(sign)?.cancel()
 
       const source = Axios.CancelToken.source()
       request.cancelToken = source.token // 请求添加 token 才可以取消
-      sourceMap.set(signature, source)
+      sourceMap.set(sign, source)
     }
   }
 })()
 
-// function requestSetToken(request: AxiosRequestConfig) {
-//   if (authStore.token) request.headers['Access-Token'] = authStore.token
-// }
-
 function handleRequest(request: AxiosRequestConfig) {
-  requestSetSignature(request)
-  // requestSetToken(request)
+  if (IS_CLIENT) {
+    autoCancelRequestBySign(request)
+  }
   return request
 }
 
--- src/util/error.ts
+++ src/util/error.ts
@@ -1,9 +1,8 @@
 import { RequestResponseData } from 'axios'
-import { DEFAULT_ERROR_MESSAGE } from '@/constant'
 
 // 从 response 中获取 message
-function getErrorMessage(response: RequestResponseData, defaultMessage = DEFAULT_ERROR_MESSAGE) {
-  return response?.message ?? defaultMessage
+function getErrorMessage(response: RequestResponseData) {
+  return response?.message ?? ''
 }
 
 export {
